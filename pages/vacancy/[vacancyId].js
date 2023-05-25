93c9217a8ab2a2c7b226d447e845eae82b4afea7
silverun <imonutiy@gmail.com>
2023-05-25 19:02:22 +0300

    Solo vac end

--- pages/vacancy/[vacancyId].js
+++ pages/vacancy/[vacancyId].js
@@ -3,11 +3,12 @@ import parse from "html-react-parser";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PulseLoader from "react-spinners/PulseLoader";
+import VacPill from "@/components/VacPill";
 
 export default function SoloVacancy() {
   const router = useRouter();
   const { vacancyId } = router.query;
-  const [vacancy, setVacancy] = useState({});
+  const [vac, setVac] = useState({});
   const [description, setDescription] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -16,7 +17,7 @@ export default function SoloVacancy() {
     const getVacancy = async () => {
       const vac = await axios.get(`/api/vacancy/${vacancyId}`);
       console.log(vac.data);
-      setVacancy(vac.data);
+      setVac(vac.data);
       setDescription(parse(vac.data.vacancyRichText));
       setIsLoading(false);
     };
@@ -30,6 +31,22 @@ export default function SoloVacancy() {
       </div>
     );
   } else {
-    return <div className="vac-description">{description}</div>;
+    return (
+      <div className="solo-vacancy">
+        <div className="solo-vacancy__pill">
+          <VacPill
+            id={vac.id}
+            profession={vac.profession}
+            townTitle={vac.town.title}
+            typeOfWorkTitle={vac.type_of_work.title}
+            payFrom={vac.payment_from}
+            payTo={vac.payment_to}
+            currency={vac.currency}
+            data-elem={`vacancy-${vac.id}`}
+          />
+        </div>
+        <div className="solo-vacancy__description">{description}</div>
+      </div>
+    );
   }
 }
